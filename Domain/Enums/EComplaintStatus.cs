4f57505500f5bab7334e30b58b0ffaea38277b76
Marley Vargas <mvargas@gmstek.com>
2024-10-02 14:35:42 -0500

    Add complaint status

--- Domain/Enums/EComplaintStatus.cs
+++ Domain/Enums/EComplaintStatus.cs
@@ -3,16 +3,21 @@ using System.ComponentModel;
 
 namespace Domain.Enums
 {
+    [Flags]
     public enum EComplaintStatus //TODo
     {
         [Description("Pendiente")]
         Pending = 1,
         [Description("En proceso de investigación")]
-        InProcess = 2,
+        InProcess = 2,       
+        [Description("Finalizada")]
+        Completed = 3,
         [Description("Desistimada")]
-        Rejected = 3,
-        [Description("SancionAplicada")]
-        sanctionApplied = 4,
+        Rejected = 4,
+        [Description("Sanción Aplicada")]
+        SanctionApplied = 5,
+        [Description("Medidas preventivas")]
+        PreventiveMeasures = 6,
 
     }
 }
