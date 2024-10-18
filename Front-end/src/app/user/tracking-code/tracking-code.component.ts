dbac09da46b8e63d8a44cf3847c241e754f80bdd
Marley Vargas <mvargas@gmstek.com>
2024-10-18 13:33:23 -0500

    add movus input

--- Front-end/src/app/user/tracking-code/tracking-code.component.ts
+++ Front-end/src/app/user/tracking-code/tracking-code.component.ts
@@ -1,15 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormsModule, FormControl } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import {  Router } from '@angular/router';
 import { ComplaintService } from '../../services/complaint.service';
-import { ResponseComplaint } from '../../../types/complaint.type';
 
 @Component({
   selector: 'app-tracking-code',
   standalone: true,
-  imports: [CommonModule,ReactiveFormsModule, FormsModule,],
+  imports: [CommonModule,ReactiveFormsModule, FormsModule],
   templateUrl: './tracking-code.component.html',
   styleUrl: './tracking-code.component.css'
 })
@@ -17,6 +16,12 @@ export class TrackingCodeComponent {
   trackForm: FormGroup;
   mensajeError: any;
   complaint: any = [];
+
+  @ViewChild('input1') input1!: ElementRef;
+  @ViewChild('input2') input2!: ElementRef;
+  @ViewChild('input3') input3!: ElementRef;
+  @ViewChild('input4') input4!: ElementRef;
+  @ViewChild('input5') input5!: ElementRef;
 
   constructor(private fb: FormBuilder,
     private route: Router,
@@ -34,16 +39,6 @@ export class TrackingCodeComponent {
     digit5: new FormControl('')
   });
 
-  moveFocus(event: any, index: number) {
-    const input = event.target.value;
-    if (input.length === 1 && index < 4) {
-      const nextInput = document.querySelectorAll('input')[index + 1];
-      if (nextInput) {
-        (nextInput as HTMLInputElement).focus();
-      }
-    }
-  }
- 
   async onSubmit() {
     let code = `${this.trackingForm.value.digit1}${this.trackingForm.value.digit2}${this.trackingForm.value.digit3}${this.trackingForm.value.digit4}${this.trackingForm.value.digit5}`;
    
@@ -57,6 +52,49 @@ export class TrackingCodeComponent {
       else
         this.mensajeError = "Código incorrecto" 
    
+  }
+
+  onInput(event: Event, inputNumber: number) {
+    const input = event.target as HTMLInputElement;
+    if (input.value.length === 1) {
+      this.focusNextInput(inputNumber);
+    }
+  }
+
+  onKeydown(event: KeyboardEvent, inputNumber: number) {
+    if (event.key === 'Backspace' && (event.target as HTMLInputElement).value === '') {
+      this.focusPreviousInput(inputNumber);
+    }
+  }
+
+  focusNextInput(inputNumber: number) {
+    const nextInput = document.querySelectorAll('input')[inputNumber + 1];
+      if (nextInput) 
+        (nextInput as HTMLInputElement).focus();
+  }
+
+  
+  focusPreviousInput(inputNumber: number) {
+    const nextInput = document.querySelectorAll('input')[inputNumber + 1];
+    if (nextInput) 
+      (nextInput as HTMLInputElement).focus();
+  }
+
+  onPaste(event: ClipboardEvent) {
+    const pastedData = event.clipboardData?.getData('text');
+    if (pastedData) {
+      const otpArray = pastedData.split('').slice(0, 5); // Solo tomar los primeros 5 caracteres si se pega más
+      this.fillOtpFields(otpArray);
+      event.preventDefault(); // Prevenir que el valor pegado se escriba en el primer input
+    }
+  }
+
+  fillOtpFields(otpArray: string[]) {
+    if (otpArray[0]) this.trackingForm.controls.digit1.setValue(otpArray[0]);
+    if (otpArray[1]) this.trackingForm.controls.digit2.setValue(otpArray[1]);
+    if (otpArray[2]) this.trackingForm.controls.digit3.setValue(otpArray[2]);
+    if (otpArray[3]) this.trackingForm.controls.digit4.setValue(otpArray[3]);
+    if (otpArray[4]) this.trackingForm.controls.digit5.setValue(otpArray[4]);
   }
 
 }
